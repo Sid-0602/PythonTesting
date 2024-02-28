ba52dfb9e8e23b93dda6bb3dff77b940e92fa7ee
Siddhant_J <sidbnjadhav@gmail.com>
2024-02-28 11:32:05 +0530

    Basic Functional tests using pytest

--- source/functions.py
+++ source/functions.py
@@ -4,3 +4,9 @@ def add(num1, num2):
 
 def multiply(num1, num2):
     return num1 * num2
+
+
+def divide(num1, num2):
+    if num2 == 0:
+        raise ZeroDivisionError
+    return num1 / num2
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,5 +1,28 @@
 import pytest
-from source.functions import add, multiply
+import source.functions as func
+
 
 def test_add():
-    pass
+    result = func.add(1, 2)
+    assert result == 3
+
+
+def test_add_strings():
+    result = func.add("I like", " burger")
+    assert result == "I like burger"
+
+
+def test_multiply():
+    result = func.multiply(3, 4)
+    assert result == 12
+
+
+def test_divide():
+    result = func.divide(5, 5)
+    assert result == 1
+
+
+# divide by zero error.
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        func.divide(10, 0)
