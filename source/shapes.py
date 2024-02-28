b3bf7b1881f2c4dd6c78253fbb7ede195d3d7f35
Siddhant_J <sidbnjadhav@gmail.com>
2024-02-28 11:56:53 +0530

    Linkted code with black

--- source/shapes.py
+++ source/shapes.py
@@ -3,14 +3,18 @@ import math
 
 class Shape:
     def area(self):
-        pass    
+        pass
+
     def perimeter(self):
         pass
-    
+
+
 class Circle(Shape):
-    def __init__(self,radius):
+    def __init__(self, radius):
         self.radius = radius
+
     def area(self):
-        return math.pi * self.radius ** 2
+        return math.pi * self.radius**2
+
     def perimeter(self):
-        return 2*math.pi*self.radius 
+        return 2 * math.pi * self.radius
